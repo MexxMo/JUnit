354ccbc826ae12a27c5974cbce96a3c45157c15c
Ildar <mexx0174@gmail.com>
2023-03-02 23:25:34 +0500

    2

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,4 +1,34 @@
 package org.example;
 
 public class User {
+
+    private final String login;
+    private final String email;
+
+
+    public User() {
+        this.login = "example";
+        this.email = "example@example.com";
+    }
+
+    public User(String login, String email) {
+        if (login.equals(email)) {
+            throw new IllegalArgumentException();
+        } else {
+            this.login = login;
+        }
+        if (email.contains("@")) {
+            this.email = email;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
